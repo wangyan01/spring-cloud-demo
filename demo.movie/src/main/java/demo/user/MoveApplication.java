5e0eb1ec4241cb57b8fa13ce75c4d6e0cff9e505
wangyan01 <wangyan01@jiuqi.com.cn>
2018-10-21 18:49:02 +0800

    wy

--- demo.movie/src/main/java/demo/user/MoveApplication.java
+++ demo.movie/src/main/java/demo/user/MoveApplication.java
@@ -2,12 +2,13 @@ package demo.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={HibernateJpaAutoConfiguration.class})
 @ImportResource(value = { "classpath:config/persistence.xml" })
 public class MoveApplication {
 	public static void main(String[] args) {
